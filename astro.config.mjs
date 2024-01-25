144f60dea3e13e6e7e13ce4ada2fd71f1296a259
Vardhan Patil <hi@vardhanpatil.com>
2024-01-25 22:05:53 +0530

    Add url to sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://www.vardhanpatil.com/',
   integrations: [mdx(), sitemap()],
 });
